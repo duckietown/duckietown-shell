d9cfcb6232d644f00e90a9e9ed5ab23269715759
Andrea F. Daniele <afdaniele@ttic.edu>
2018-08-01 11:46:47 -0500

    added placeholder commands

--- commands/aido18/init/command.py
+++ commands/aido18/init/command.py
@@ -13,11 +13,11 @@ class DTCommand(DTCommandAbs):
         else:
             shell.config['aido18'] = {}
         # get app_id
-        msg = 'Your Application ID%s: ' % ('[%s]'%app_id if app_id is not None else '')
+        msg = 'Your Application ID%s: ' % (' [%s]'%app_id if app_id is not None else '')
         app_id_in = raw_input(msg)
         app_id = app_id_in if len(app_id_in.strip()) > 0 else app_id
         # get app_secret
-        msg = 'Your Application Secret%s: ' % ('[%s]'%app_secret if app_secret is not None else '')
+        msg = 'Your Application Secret%s: ' % (' [%s]'%app_secret if app_secret is not None else '')
         app_secret_in = raw_input(msg)
         app_secret = app_secret_in if len(app_secret_in.strip()) > 0 else app_secret
         # store app_id and app_secret
