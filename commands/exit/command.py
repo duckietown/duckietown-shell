4defcbd544c95c0cabd83b6b736c971a2d498bef
Andrea F. Daniele <afdaniele@ttic.edu>
2018-08-01 12:12:37 -0500

    moved commands to submodule

--- commands/exit/command.py
+++
@@ -1,8 +0,0 @@
-from dt_shell import DTCommandAbs
-
-class DTCommand(DTCommandAbs):
-
-    @staticmethod
-    def command(shell, line):
-        print 'Bye bye!'
-        exit()
--- commands/version/command.py
+++
@@ -1,7 +0,0 @@
-from dt_shell import DTCommandAbs
-
-class DTCommand(DTCommandAbs):
-
-    @staticmethod
-    def command(shell, line):
-        print "%s: v%s" % (shell.NAME, shell.VERSION)
