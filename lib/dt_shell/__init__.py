50edc1c7c0e1a553199cab4d068a412e1f9bbc15
Andrea Censi <acensi@ethz.ch>
2018-10-09 13:00:53 +0200

    Bump version: 3.0.9 → 3.0.10

--- lib/dt_shell/__init__.py
+++ lib/dt_shell/__init__.py
@@ -12,7 +12,7 @@ logging.basicConfig()
 dtslogger = logging.getLogger('dts')
 dtslogger.setLevel(logging.DEBUG)
 
-__version__ = '3.0.9'
+__version__ = '3.0.10'
 
 dtslogger.info('duckietown-shell %s' % __version__)
 
