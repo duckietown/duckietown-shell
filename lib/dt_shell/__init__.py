3ac4d5dcad723409a89783751783d2b1dd9850ca
Andrea Censi <acensi@ethz.ch>
2018-10-21 08:45:43 +0200

    Bump version: 3.0.24 → 3.0.25

--- lib/dt_shell/__init__.py
+++ lib/dt_shell/__init__.py
@@ -12,7 +12,7 @@ logging.basicConfig()
 dtslogger = logging.getLogger('dts')
 dtslogger.setLevel(logging.DEBUG)
 
-__version__ = '3.0.24'
+__version__ = '3.0.25'
 
 class OtherVersions(object):
     name2versions = {}
