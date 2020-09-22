846dc837466141ad3a15baa05ee5841c7afc0191
Andrea Censi <acensi@ethz.ch>
2020-09-22 12:59:03 +0200

    Bump version: 5.1.10 → 5.1.11

--- lib/dt_shell/__init__.py
+++ lib/dt_shell/__init__.py
@@ -6,7 +6,7 @@ logging.basicConfig()
 dtslogger = logging.getLogger("dts")
 dtslogger.setLevel(logging.INFO)
 
-__version__ = "5.1.10"
+__version__ = "5.1.11"
 
 dtslogger.info("duckietown-shell %s" % __version__)
 
