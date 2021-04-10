2d172dad6b8acac303454c8b6c4723e770b69fd4
Andrea F. Daniele <afdaniele@ttic.edu>
2021-04-09 22:48:33 -0500

    Bump version: 5.2.6 → 5.2.7

--- lib/dt_shell/__init__.py
+++ lib/dt_shell/__init__.py
@@ -6,7 +6,7 @@ logging.basicConfig()
 dtslogger = logging.getLogger("dts")
 dtslogger.setLevel(logging.INFO)
 
-__version__ = "5.2.6"
+__version__ = "5.2.7"
 
 dtslogger.debug(f"duckietown-shell {__version__}")
 
