ed35e39b825413708326247b3d4281bae8094966
Andrea Censi <acensi@ethz.ch>
2021-06-17 23:14:31 +0200

    Bump 5.2.8 -> 5.2.9 [ci skip]

--- lib/dt_shell/__init__.py
+++ lib/dt_shell/__init__.py
@@ -6,7 +6,7 @@ logging.basicConfig()
 dtslogger = logging.getLogger("dts")
 dtslogger.setLevel(logging.INFO)
 
-__version__ = "5.2.8"
+__version__ = "5.2.9"
 
 dtslogger.debug(f"duckietown-shell {__version__}")
 
