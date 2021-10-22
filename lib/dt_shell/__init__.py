df4ba12eb7d8c49456d3a572ee502a318324648a
Andrea Censi <acensi@ethz.ch>
2021-10-22 12:01:54 +0200

    Bump 5.2.16 -> 5.2.17  (automatic)

--- lib/dt_shell/__init__.py
+++ lib/dt_shell/__init__.py
@@ -6,7 +6,7 @@ logging.basicConfig()
 dtslogger = logging.getLogger("dts")
 dtslogger.setLevel(logging.INFO)
 
-__version__ = "5.2.16"
+__version__ = "5.2.17"
 
 dtslogger.debug(f"duckietown-shell {__version__}")
 
