49ccf8d49a77efc47e47f8c2c7fe36ef1e935c8a
Andrea F. Daniele <afdaniele@duckietown.com>
2023-04-20 19:16:50 -0400

    bumped deps

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ install_requires = [
     'dt-authentication-{}'.format(distro),
     'dt-data-api-{}>=1.2.0'.format(distro),
     "pip",
-    "dockertown>=0.2.1",
+    "dockertown>=0.2.2",
     "dtproject",
 ]
 
