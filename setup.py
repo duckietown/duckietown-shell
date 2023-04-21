a49ce9332ce96dc61220749b08477f72a826362f
Andrea F. Daniele <afdaniele@duckietown.com>
2023-04-20 22:51:49 -0400

    bumped deps

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ install_requires = [
     'dt-data-api-{}>=1.2.0'.format(distro),
     "pip",
     "dockertown>=0.2.2",
-    "dtproject>=0.0.2",
+    "dtproject>=0.0.3",
 ]
 
 system_version = tuple(sys.version_info)[:3]
